f93e0be2dda0a11774d40483424a0dfebe0025e0
aPurpleBerry <2454196228@qq.com>
2024-07-19 15:55:45 +0800

    完结-加入我们

---
+++ newsWeb/src/views/home-page/page5/discard-index.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div class="shell">
+      <div class="box">
+        <img src="https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+        <span>Image</span>
+      </div>
+      <div class="box">
+        <img src="" alt="">
+        <span>Image2</span>
+      </div>
+      <div class="box">
+        <img src="" alt="">
+        <span>Image3</span>
+      </div>
+      <div class="box">
+        <img src="" alt="">
+        <span>Image4</span>
+      </div>
+      <div class="box">
+        <img src="" alt="">
+        <span>Image5</span>
+      </div>
+    </div>
+    <div class="footer">
+      <h1>Copyright ©2024 APURPLEBERRY. <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2024264343号</a></h1>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.shell {
+  width: 40%;
+  height: 400px;
+  display: flex;
+  .box{
+    // width: 150px;
+    // height: 500px;
+    // margin-left: 20px;
+    background-color: red;
+    flex: 1;
+    // box-shadow: 10px 10px 20px rgba(0,0,0,.5);
+    // border-radius: 20px;
+    border: 5px solid #fff;
+    overflow: hidden;
+    transition: 1s;
+
+  }
+  .box>img {
+    width: 200%;
+    object-fit: cover;
+    transition: 1s;
+  }
+  .box>span {
+    font: 200 45px;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .box:hover{
+    flex-basis: 40%;
+  }
+  .box:hover>img {
+    width: 100%;
+    height: 100%;
+  }
+}
+.footer {
+  width: 100%;
+  height: 50px;
+  background-color: #fff;
+  opacity: 0.8;
+  position: fixed;
+  bottom: 0;
+  h1 {
+    width: 350px;
+    line-height: 50px;
+    font-size: 12px;
+    margin-left: 40%;
+    margin: 0 auto;
+  }
+}
+</style>
--- newsWeb/src/views/home-page/page5/index.vue
+++ newsWeb/src/views/home-page/page5/index.vue
@@ -1,74 +1,207 @@
 <template>
-  <div>
-    <div class="shell">
+  <div class="career-tab">
+    <div class="onecareer" style="background-color: rgb(43,115,175);">校园招聘</div>
+    <div class="onecareer" style="color: rgb(43,115,175);;">社会招聘</div>
+  </div>
+  <div class="scroll-container">
+    <div class="scroll-content">
       <div class="box">
-        <img src="https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
-        <span>Image</span>
+        <div class="mask"><h1>技术研发类</h1></div>
+        <img src="@/assets/career_7.jpg" alt="">
+        <div class="cover"><h2>技术研发类</h2></div>
       </div>
       <div class="box">
-        <img src="" alt="">
-        <span>Image2</span>
+        <div class="mask"><h1>美术类</h1></div>
+        <img src="@/assets/career_5.jpg" alt="">
+        <div class="cover"><h2>美术类</h2></div>
       </div>
       <div class="box">
-        <img src="" alt="">
-        <span>Image3</span>
+        <div class="mask"><h1>策划类</h1></div>
+        <img src="@/assets/career_2.jpg" alt="">
+        <div class="cover"><h2>策划类</h2></div>
       </div>
       <div class="box">
-        <img src="" alt="">
-        <span>Image4</span>
+        <div class="mask"><h1>市场运营类</h1></div>
+        <img src="@/assets/career_3.jpg" alt="">
+        <div class="cover"><h2>市场运营类</h2></div>
       </div>
       <div class="box">
-        <img src="" alt="">
-        <span>Image5</span>
+        <div class="mask"><h1>动画CG类</h1></div>
+        <img src="@/assets/career_4.jpg" alt="">
+        <div class="cover"><h2>动画CG类</h2></div>
+      </div>
+      <div class="box">
+        <div class="mask"><h1>平台类</h1></div>
+        <img src="@/assets/career_1.jpg" alt="">
+        <div class="cover"><h2>平台类</h2></div>
+      </div>
+      <div class="box">
+        <div class="mask"><h1>音频类</h1></div>
+        <img src="@/assets/career_6.jpg" alt="">
+        <div class="cover"><h2>音频类</h2></div>
+      </div>
+      <div class="box">
+        <div class="mask"><h1>IP开发类</h1></div>
+        <img src="@/assets/career_0.jpg" alt="">
+        <div class="cover"><h2>IP开发类</h2></div>
       </div>
     </div>
-    <div class="footer">
-      <h1>Copyright ©2024 APURPLEBERRY. <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2024264343号</a></h1>
-    </div>
+  </div>
+  <div class="careertitle">
+    CAREER
+  </div>
+  <div class="footer">
+    <h1>Copyright ©2024 APURPLEBERRY. <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2024264343号</a></h1>
   </div>
 </template>
 
 <script setup lang="ts">
+import { nextTick } from 'vue';
 
+nextTick(()=>{
+  let boxs = document.querySelectorAll('.box')
+  console.log(boxs);
+
+  for(let i=0; i<boxs.length; i++) {
+    boxs[i].addEventListener('mouseenter',()=>{
+      let masks = document.querySelectorAll('.mask')
+      //@ts-ignore
+      masks[i].style.opacity = 0;
+    })
+
+    boxs[i].addEventListener('mouseleave',()=>{
+      let masks = document.querySelectorAll('.mask')
+      //@ts-ignore
+      masks[i].style.opacity = 1;
+    })
+  }
+})
 </script>
 
-<style scoped lang="scss">
-.shell {
-  width: 40%;
-  height: 400px;
+<style scoped>
+.careertitle {
+  margin: 0px 0px 15px 120px;
+  color: rgb(199,199,199);
+  font-size: 5rem;
+  
+}
+.career-tab {
+  width: 200px;
+  height: 25px;
+  /* background-color: red; */
+  /* margin-left: 120px;
+  margin-top: 50px; */
+  margin: 50px 0px 15px 120px;
   display: flex;
-  .box{
-    // width: 150px;
-    // height: 500px;
-    // margin-left: 20px;
-    background-color: red;
-    flex: 1;
-    // box-shadow: 10px 10px 20px rgba(0,0,0,.5);
-    // border-radius: 20px;
-    border: 5px solid #fff;
-    overflow: hidden;
-    transition: 1s;
+}
+.onecareer {
+  width: 100px;
+  height: 100%;
+  font-size: 14px;
+  color: #fff;
+  line-height: 25px;
+  padding-left: 20px;
+  cursor: pointer;
+}
 
-  }
-  .box>img {
-    width: 200%;
-    object-fit: cover;
-    transition: 1s;
-  }
-  .box>span {
-    font: 200 45px;
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .box:hover{
-    flex-basis: 40%;
-  }
-  .box:hover>img {
-    width: 100%;
+.mask {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  /* background-color: rgba(255, 255, 255, 0.5);  */
+
+  background: rgba(255, 255, 255, 0.5);
+  clip-path: polygon(0 0, 600% 0, 0 80%, 0 0); 
+}
+.mask h1 {
+  writing-mode: vertical-lr;
+  font-size: 15px;
+  font-weight: 600;
+  /* color: rgb(102,102,102); */
+  color: rgb(43,115,175);
+  margin-top: 100px;
+  margin-left: 10px;
+}
+.scroll-container {
+  width: 850px;
+  height: 400px;
+  /* border: 5px solid #fff; */
+  /* display: flex; */
+  background-color: #fff;
+  padding: 12px 10px 12px 10px;
+  overflow-x: scroll;
+  margin-left: 120px;
+}
+.scroll-content {
+  width: 1000px;
+  height: 100%;
+  /* background-color: red; */
+  display: flex;
+  /* overflow-x: scroll; */
+}
+.box {
+  flex: 0 0 auto;
+  width: 100px;
+  height: 100%;
+  margin-right: 5px;
+  /* background-color: yellow; */
+  transition: .5s;
+  position: relative;
+}
+.cover {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 20%;
     height: 100%;
-  }
+    background-color: rgb(17, 92, 154);
+    opacity: 0;
+    transform: scale(0);
+}
+.cover h2 {
+  writing-mode: vertical-lr;
+  /* background-color: red; */
+  width: 30px;
+  color: white;
+  font-size: 20px;
+  font-weight: bolder;
+  margin-top: 100px;
+  margin-left: 10px;
+}
+
+.box:hover .cover {
+    opacity: 0.4;
+    transform: scale(1);
+}
+
+.box>img {
+  /* max-height: 100%;
+  height: auto; */
+  height: 100%;
+  width: auto;
+  overflow: hidden;
+  object-fit: cover;
+}
+.box:hover{
+  width: 250px;
+}
+
+::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
+::-webkit-scrollbar-track {
+  background-color: rgb(204,204,204);
+}
+::-webkit-scrollbar-thumb {
+  background: #2b73af;
+  border-radius: 0;
+}
+::-webkit-scrollbar-thumb:hover {
+  background: rgb(151,200,220);
+
 }
 .footer {
   width: 100%;
@@ -77,12 +210,12 @@
   opacity: 0.8;
   position: fixed;
   bottom: 0;
-  h1 {
+}
+.footer h1 {
     width: 350px;
     line-height: 50px;
     font-size: 12px;
     margin-left: 40%;
     margin: 0 auto;
   }
-}
 </style>
